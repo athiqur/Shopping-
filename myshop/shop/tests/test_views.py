670a43919af11956ae86f71e209700e8dd197047
athiqur <116162887+athiqur@users.noreply.github.com>
2023-01-04 16:35:18 +0530

    Add list view for product (#3)
    * Add list view for product
    
    * Refactor : views
    
    * Add requirements.txt
    
    * Refactor:testcases
    
    * Refactor: assert in testcase
    
    Co-authored-by: Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>

---
+++ myshop/shop/tests/test_views.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.urls import reverse
+from shop.tests.test_model_mixin import ModelMixin
+
+
+class TestProductList(ModelMixin, TestCase):
+    def test_product_list_view_returns_correct_context_when_the_category_slug_is_valid(
+        self,
+    ):
+        response = self.client.get(
+            reverse("shop:product_list_by_category", args=[self.category])
+        )
+        self.assertEquals(response.context["products"][0], self.product)
+
+    def test_product_list_view_returns_error_status_when_the_category_slug_is_invalid(
+        self,
+    ):
+        response = self.client.get(
+            reverse("shop:product_list_by_category", args=["invalid-slug"])
+        )
+        self.assertEqual(response.status_code, 404)
