ab2637c4d0bddb84fd5ff43cf72b58fc8a3a2b38
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2023-01-04 11:54:45 +0530

    Refactor:testcases

--- myshop/shop/tests/test_views.py
+++ myshop/shop/tests/test_views.py
@@ -4,18 +4,18 @@ from shop.tests.test_model_mixin import ModelMixin
 
 
 class TestProductList(ModelMixin, TestCase):
-    def test_product_list_view_returns_success_status_code_when_the_category_slug_is_valid(
+    def test_product_list_view_returns_correct_context_when_the_category_slug_is_valid(
         self,
     ):
         response = self.client.get(
             reverse("shop:product_list_by_category", args=[self.category])
         )
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.context["products"][0], self.product)
 
-    def test_product_list_view_returns_error_status_code_when_the_category_slug_is_invalid(
+    def test_product_list_view_returns_empty_context_when_the_category_slug_is_invalid(
         self,
     ):
         response = self.client.get(
             reverse("shop:product_list_by_category", args=["invalid-slug"])
         )
-        self.assertEquals(response.status_code, 404)
+        self.assertIsNone(response.context["None"])
