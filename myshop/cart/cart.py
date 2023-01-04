ed09041dcc037aa2582ba7b97d050a56bc529cdc
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2023-01-04 14:16:49 +0530

    Refactor: Cart methods

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -12,32 +12,28 @@ class Cart(object):
         self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False):
-        product_id = str(product.id)
-
-        if product_id not in self.cart:
-            self.cart[product_id] = {
+        if str(product.id) not in self.cart:
+            self.cart[str(product.id)] = {
                 "quantity": 0,
                 "price": str(product.price),
             }
 
         if override_quantity:
-            self.cart[product_id]["quantity"] = quantity
+            self.cart[str(product.id)]["quantity"] = quantity
         else:
-            self.cart[product_id]["quantity"] += quantity
+            self.cart[str(product.id)]["quantity"] += quantity
         self.save()
 
     def remove(self, product):
-        product_id = str(product.id)
-        if product_id in self.cart:
-            del self.cart[product_id]
+        if str(product.id) in self.cart:
+            del self.cart[str(product.id)]
             self.save()
 
     def save(self):
         self.session.modified = True
 
     def __iter__(self):
-        product_ids = self.cart.keys()
-        products = Product.objects.filter(id__in=product_ids)
+        products = Product.objects.filter(id__in=self.cart.keys())
         cart = self.cart.copy()
 
         for product in products:
@@ -57,6 +53,6 @@ class Cart(object):
             for item in self.cart.values()
         )
 
-    def clear(self):
+    def clear_session(self):
         del self.session[settings.CART_SESSION_ID]
         self.save()
