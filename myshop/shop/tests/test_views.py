1a6fbf41977c485627bc8ccce3c1f989b30731a5
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2023-01-04 11:37:32 +0530

    Add detail view for product

--- myshop/shop/tests/test_views.py
+++ myshop/shop/tests/test_views.py
@@ -19,3 +19,24 @@ class TestProductList(ModelMixin, TestCase):
             reverse("shop:product_list_by_category", args=["invalid-slug"])
         )
         self.assertEqual(response.status_code, 404)
+
+
+class TestProductDetail(ModelMixin, TestCase):
+    def test_product_detail_view_returns_success_view_with_correct_context(
+        self,
+    ):
+        response = self.client.get(
+            reverse(
+                "shop:product_detail",
+                args=[self.product.id, self.product.slug],
+            )
+        )
+        self.assertEquals(response.context["product"], self.product)
+
+    def test_product_detail_view_returns_empty_context_when_the_product_is_not_found(
+        self,
+    ):
+        response = self.client.get(
+            reverse("shop:product_detail", args=[1000, "invalid-slug"])
+        )
+        self.assertIsNone(response.context["None"])
--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
         views.product_list,
         name="product_list_by_category",
     ),
+    path("<int:id>/<slug:slug>/", views.product_detail, name="product_detail"),
 ]
